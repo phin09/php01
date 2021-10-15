0f2307152c9787dd2cbaa5676cfce50a1a27e861
phin <seongyeon.kang@solarconnect.kr>
2021-10-15 18:25:13 +0900

    routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/a/{test?}', function ($test='default') {
+    return $test;
+});
+
+// 아래 같이 하면 test를 안넣었을 때 page not found
+Route::get('/{test?}/b', function ($test='default') {
+    return $test;
+});
+
+Route::get('/home1', [
+    'as' => 'home1', // 라우트 이름
+    function () {
+        return 'Home!';
+    }
+]);
+Route::get('/home2', function () {
+    return redirect(route('home1')); // home1으로 이동. url도 /home1으로 바뀜.
+});
