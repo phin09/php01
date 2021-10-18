50323cc033191651e476d439d331acb3fa3d1145
phin <seongyeon.kang@solarconnect.kr>
2021-10-18 20:23:36 +0900

    sub-view

--- resources/views/child01.blade.php
+++ resources/views/child01.blade.php
@@ -8,4 +8,13 @@
 
 @section('content')
     <p>child 뷰의 'content' 섹션입니다.</p>
+    @include('partials.footer')
 @endsection {{--또는 @stop--}}
+
+{{--이름이 겹치는 section을 테스트해보았지만 child 뷰의 내용과 조각뷰의 내용 둘다 뜨지 않음--}}
+{{--@parent도 소용없이 둘다 뜨지 않음--}}
+{{--@section('script')--}}
+{{--    <script>--}}
+{{--        alert("child 뷰의 script 섹션입니다.")--}}
+{{--    </script>--}}
+{{--@endsection--}}
